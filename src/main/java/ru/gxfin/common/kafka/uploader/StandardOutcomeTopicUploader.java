e0d60afb6b1506a68970d4da04f411bcc9569c94
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-09-10 17:17:37 +0300

    v1.0.18. StandardOutcomeTopicUploader.createPackage - сделал public.

--- src/main/java/ru/gxfin/common/kafka/uploader/StandardOutcomeTopicUploader.java
+++ src/main/java/ru/gxfin/common/kafka/uploader/StandardOutcomeTopicUploader.java
@@ -1,10 +1,17 @@
 package ru.gxfin.common.kafka.uploader;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.gxfin.common.data.DataObject;
+import ru.gxfin.common.data.DataPackage;
 
 @SuppressWarnings("unused")
 public class StandardOutcomeTopicUploader extends AbstractOutcomeTopicUploader {
     public StandardOutcomeTopicUploader(ObjectMapper objectMapper) {
         super(objectMapper);
+    }
+
+    @Override
+    public <O extends DataObject, P extends DataPackage<O>> P createPackage(OutcomeTopicUploadingDescriptor<O, P> descriptor) throws Exception {
+        return super.createPackage(descriptor);
     }
 }
