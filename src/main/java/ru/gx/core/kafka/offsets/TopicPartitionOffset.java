cbc9eda007df4c2a74588044fe821bd054770a18
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-12-07 21:30:33 +0300

    v1.2.5. Объединение TopicsOffsetsLoader и TopicsOffsetsSaver в TopicsOffsetsController.

--- src/main/java/ru/gx/core/kafka/offsets/TopicPartitionOffset.java
+++ src/main/java/ru/gx/core/kafka/offsets/TopicPartitionOffset.java
@@ -1,5 +1,7 @@
 package ru.gx.core.kafka.offsets;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,12 +10,17 @@ import org.jetbrains.annotations.NotNull;
 
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = false)
-@ToString
+@ToString(callSuper = true)
 public class TopicPartitionOffset extends PartitionOffset {
     @Getter
     private final String topic;
 
-    public TopicPartitionOffset(@NotNull final String topic, final int partition, final long offset) {
+    @JsonCreator
+    public TopicPartitionOffset(
+            @JsonProperty("topic") @NotNull final String topic,
+            @JsonProperty("partition") final int partition,
+            @JsonProperty("offset") final long offset
+    ) {
         super(partition, offset);
         this.topic = topic;
     }
--- src/main/java/ru/gx/core/kafka/offsets/TopicsOffsetsController.java
+++ src/main/java/ru/gx/core/kafka/offsets/TopicsOffsetsController.java
@@ -7,10 +7,11 @@ import java.util.Collection;
 
 /**
  * Задачи класса, реализующего данный интерфейс состоят в том, чтобы уметь читать стартовые смещения из
- * места источника хранения (это может быть локальная БД, а может быть и внешний сервис, предоставляющий данную услугу).
+ * места источника хранения (это может быть локальная БД, а может быть и внешний сервис, предоставляющий данную услугу),
+ * а также сохранять туда же текущие смещения.
  */
 @SuppressWarnings("unused")
-public interface TopicsOffsetsLoader {
+public interface TopicsOffsetsController {
     /**
      * Загрузка смещений и выдача результата в виде коллекции.
      * @param direction Направление топиков, для которых надо прочитать смещения.
@@ -18,4 +19,10 @@ public interface TopicsOffsetsLoader {
      * @return Список смещений.
      */
     Collection<TopicPartitionOffset> loadOffsets(@NotNull final ChannelDirection direction, @NotNull final String serviceName);
+
+    /**
+     * Сохранение всех смещений, переданным в списке смещений.
+     * @param offsets Список смещений, который требуется сохранить.
+     */
+    void saveOffsets(@NotNull final ChannelDirection direction, @NotNull final String serviceName, @NotNull final Collection<TopicPartitionOffset> offsets);
 }
